6a627609cb2d44ec437b269433fb013c335ab2c6
Yessimkul <Acc2.avabrands@gmail.com>
2022-11-27 12:26:36 +0600

    Добавил домашнее задание до 43

---
+++ Task41/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите числа через пробел:");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine($"Количество чисел больше 0 = {MoreThanZero(array)}");
+
+int[] GetArrayFromString(string arrayString)
+{
+    string[] nums=arrayString.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+    int[] result=new int[nums.Length];
+    for(int i=0; i<result.Length; i++)
+    {
+        result[i]= int.Parse(nums[i]);
+    }
+    return result;
+}
+
+int MoreThanZero(int[] InArray)
+{
+    int counter = 0;
+    foreach(int item in InArray)
+    {
+        if(item>0) counter++;
+    }
+    return counter;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите b1,k1,b2,k2 через пробел");
+string[] nums = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+double[] point = GetPoint(double.Parse(nums[0]),double.Parse(nums[1]),double.Parse(nums[2]),double.Parse(nums[3]));
+WriteLine($"[{String.Join(";",point)}]");
+
+double[] GetPoint(double b1, double k1, double b2, double k2)
+{
+    double[] point=new double[2];
+    point[0]=(b2-b1)/(k1-k2);
+    point[1]=point[0]*k1 +b1;
+    
+    return point;
+}
